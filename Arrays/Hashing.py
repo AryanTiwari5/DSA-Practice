1610c2b4b35b16b065fadfe1ec2a05a7564ad9ad
Aryan Tiwari <tiwariaryan505@gmail.com>
2026-08-30 22:52:38 +0530

    Hashing

---
+++ Arrays/Hashing.py
@@ -0,0 +1,25 @@
+#Question: n = [5 3 2 2 1 5 5 7 5 10]
+# m = [10 111 1 9 5 67 2]
+#Output = [1 0 1 0 3 0 2]
+#Brute Force solution
+n = [5, 3, 2, 2, 1, 5, 5, 7, 5, 10]
+m = [10, 111, 1, 9, 5, 67, 2]
+#for num in m:
+   # count = 0
+   ## for x in n:
+   #     if x == num:
+   #         count += 1
+  #  print(count)
+
+#Optimal solution
+hash_list = [0]*11
+print(hash_list)
+
+for num in n:
+    hash_list[num] += 1
+
+for num in m:
+    if num<1 or num>10:
+        print(0)
+    else:
+        print(hash_list[num])
